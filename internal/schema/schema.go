e874dd8755eb4fbe4ed8ad910dc62e97bd40e665
Dmitry Solovov <cyradin@protonmail.com>
2022-03-18 01:00:59 +0300

    Add method to read schema from json file

--- internal/schema/schema.go
+++ internal/schema/schema.go
@@ -1,5 +1,16 @@
 package schema
 
+import (
+	"io/ioutil"
+	"os"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+var (
+	json = jsoniter.ConfigCompatibleWithStandardLibrary
+)
+
 type Type string
 
 const (
@@ -15,20 +26,45 @@ func (t Type) Valid() bool {
 }
 
 type Field struct {
-	Name     string
-	Type     Type
-	Source   string
-	Required bool
+	Name     string `json:"name"`
+	Type     Type   `json:"type"`
+	Source   string `json:"source"`
+	Required bool   `json:"required"`
 
-	Children []Field
+	Children []Field `json:"children"`
 }
 
 type Schema struct {
-	Fields []Field
+	Fields []Field `json:"fields"`
 }
 
 func New(fields []Field) *Schema {
 	return &Schema{
 		Fields: fields,
 	}
+}
+
+func NewFromJSON(data []byte) (*Schema, error) {
+	result := new(Schema)
+	err := json.Unmarshal(data, result)
+	if err != nil {
+		return nil, err
+	}
+
+	return result, nil
+}
+
+func NewFromFile(src string) (*Schema, error) {
+	f, err := os.Open(src)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		return nil, err
+	}
+
+	return NewFromJSON(data)
 }
