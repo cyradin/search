75ed82e12824b567903a9df8355d456bf1765b3d
Dmitry Solovov <cyradin@protonmail.com>
2022-03-22 01:54:27 +0300

    Add Field interface to hide field logic inside the package

--- internal/index/field/field.go
+++ internal/index/field/field.go
@@ -1,5 +1,10 @@
 package field
 
+import (
+	"context"
+	"fmt"
+)
+
 type Type string
 
 const (
@@ -15,5 +20,21 @@ func (t Type) Valid() bool {
 }
 
 type Field interface {
+	// Type returns field type
 	Type() Type
+	// AddValue add document field value
+	AddValue(id uint32, value interface{}) error
+	// AddValueSync add document field value synchronously
+	AddValueSync(id uint32, value interface{}) error
+}
+
+func NewField(ctx context.Context, fieldType Type) (Field, error) {
+	switch fieldType {
+	case TypeBool:
+		return NewBool(ctx), nil
+	case TypeKeyword:
+		return NewKeyword(ctx), nil
+	}
+
+	return nil, fmt.Errorf("unknown field type %q", fieldType)
 }
