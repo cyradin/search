18f2335ac8aa988ea3a94c5a88b212e968785fab
Dmitry Solovov <cyradin@protonmail.com>
2022-03-27 03:05:58 +0300

    Fix index add field

--- internal/index/index.go
+++ internal/index/index.go
@@ -37,11 +37,36 @@ func New(ctx context.Context, s *schema.Schema) (*Index, error) {
 }
 
 func (i *Index) addField(ctx context.Context, f schema.Field) error {
-	ff, err := field.NewField(ctx, f.Type)
-	if err != nil {
-		return err
+	switch f.Type {
+	case field.TypeBool:
+		i.fields[f.Name] = field.NewBool(ctx)
+	case field.TypeKeyword:
+		i.fields[f.Name] = field.NewKeyword(ctx)
+	case field.TypeText:
+		i.fields[f.Name] = field.NewText(ctx) // @todo pass analyzers from schema
+	// @todo implement slice type
+	// case field.TypeSlice:
+	// 	i.fields[f.Name] = field.NewSlice(ctx)
+	// @todo implement map type
+	// case field.TypeNap:
+	// 	i.fields[f.Name] = field.NewMap(ctx)
+	case field.TypeUnsignedLong:
+		i.fields[f.Name] = field.NewUnsignedLong(ctx)
+	case field.TypeLong:
+		i.fields[f.Name] = field.NewLong(ctx)
+	case field.TypeInteger:
+		i.fields[f.Name] = field.NewInteger(ctx)
+	case field.TypeShort:
+		i.fields[f.Name] = field.NewShort(ctx)
+	case field.TypeByte:
+		i.fields[f.Name] = field.NewByte(ctx)
+	case field.TypeDouble:
+		i.fields[f.Name] = field.NewDouble(ctx)
+	case field.TypeFloat:
+		i.fields[f.Name] = field.NewFloat(ctx)
+	default:
+		return fmt.Errorf("invalid field type %q", f.Type)
 	}
-	i.fields[f.Name] = ff
 
 	return nil
 }
