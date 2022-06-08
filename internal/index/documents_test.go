16e687ffe0eac6e4e927892227e5e13d12672e1a
Dmitry Solovov <cyradin@protonmail.com>
2022-06-09 01:54:01 +0300

    Change document id type to uint32

--- internal/index/documents_test.go
+++ internal/index/documents_test.go
@@ -11,98 +11,71 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-var _ Storage[entity.DocSource] = (*testDocStorage)(nil)
+var _ Storage[uint32, entity.DocSource] = (*testDocStorage)(nil)
 
 type testDocStorage struct {
-	one    func(id string) (Document[entity.DocSource], error)
-	multi  func(ids ...string) ([]Document[entity.DocSource], error)
-	all    func() (<-chan Document[entity.DocSource], <-chan error)
-	insert func(id string, doc entity.DocSource) (string, error)
-	update func(id string, doc entity.DocSource) error
-	delete func(id string) error
+	one    func(id uint32) (Document[uint32, entity.DocSource], error)
+	multi  func(ids ...uint32) ([]Document[uint32, entity.DocSource], error)
+	all    func() (<-chan Document[uint32, entity.DocSource], <-chan error)
+	insert func(id uint32, doc entity.DocSource) (uint32, error)
+	update func(id uint32, doc entity.DocSource) error
+	delete func(id uint32) error
 }
 
-func (s *testDocStorage) One(id string) (Document[entity.DocSource], error) {
+func (s *testDocStorage) One(id uint32) (Document[uint32, entity.DocSource], error) {
 	return s.one(id)
 }
-func (s *testDocStorage) Multi(ids ...string) ([]Document[entity.DocSource], error) {
+func (s *testDocStorage) Multi(ids ...uint32) ([]Document[uint32, entity.DocSource], error) {
 	return s.multi(ids...)
 }
-func (s *testDocStorage) All() (<-chan Document[entity.DocSource], <-chan error) {
+func (s *testDocStorage) All() (<-chan Document[uint32, entity.DocSource], <-chan error) {
 	return s.all()
 }
-func (s *testDocStorage) Insert(id string, doc entity.DocSource) (string, error) {
+func (s *testDocStorage) Insert(id uint32, doc entity.DocSource) (uint32, error) {
 	return s.insert(id, doc)
 }
-func (s *testDocStorage) Update(id string, doc entity.DocSource) error {
+func (s *testDocStorage) Update(id uint32, doc entity.DocSource) error {
 	return s.update(id, doc)
 }
 
-func (s *testDocStorage) Delete(id string) error {
+func (s *testDocStorage) Delete(id uint32) error {
 	return s.delete(id)
 }
 
 func Test_Documents_Add(t *testing.T) {
 	data := []struct {
 		name         string
-		guid         string
-		generator    func() string
-		sourceInsert func(id string, doc entity.DocSource) (string, error)
+		sourceInsert func(id uint32, doc entity.DocSource) (uint32, error)
 		source       entity.DocSource
 		erroneous    bool
-		expected     string
+		expected     uint32
 	}{
 		{
-			name: "empty_id",
-			guid: "",
-			generator: func() string {
-				return "id"
-			},
+			name:   "source_insert_err",
 			source: entity.DocSource{"v": true},
-			sourceInsert: func(guid string, doc entity.DocSource) (string, error) {
-				return "id", nil
-			},
-			erroneous: false,
-			expected:  "id",
-		},
-		{
-			name: "source_insert_err",
-			guid: "id",
-			generator: func() string {
-				return "id"
-			},
-			source: entity.DocSource{"v": true},
-			sourceInsert: func(guid string, doc entity.DocSource) (string, error) {
-				return "", fmt.Errorf("err")
+			sourceInsert: func(id uint32, doc entity.DocSource) (uint32, error) {
+				return 0, fmt.Errorf("err")
 			},
 			erroneous: true,
-			expected:  "id",
+			expected:  1,
 		},
 		{
-			name: "field_value_set_err",
-			guid: "id",
-			generator: func() string {
-				return "id"
-			},
+			name:   "field_value_set_err",
 			source: entity.DocSource{"v": "1"},
-			sourceInsert: func(guid string, doc entity.DocSource) (string, error) {
-				return "id", nil
+			sourceInsert: func(id uint32, doc entity.DocSource) (uint32, error) {
+				return 1, nil
 			},
 			erroneous: true,
-			expected:  "id",
+			expected:  1,
 		},
 		{
-			name: "ok",
-			guid: "id",
-			generator: func() string {
-				return "id"
-			},
+			name:   "ok",
 			source: entity.DocSource{"v": true},
-			sourceInsert: func(guid string, doc entity.DocSource) (string, error) {
-				return "id", nil
+			sourceInsert: func(id uint32, doc entity.DocSource) (uint32, error) {
+				return 1, nil
 			},
 			erroneous: false,
-			expected:  "id",
+			expected:  1,
 		},
 	}
 
@@ -124,7 +97,7 @@ func Test_Documents_Add(t *testing.T) {
 			)
 			require.NoError(t, err)
 
-			guid, err := docs.Add(d.guid, d.source)
+			guid, err := docs.Add(0, d.source)
 			if d.erroneous {
 				require.Error(t, err)
 			} else {
