d3e2916d91b2acc1025b1f52de98b096f8489bcc
Dmitry Solovov <cyradin@protonmail.com>
2022-07-26 01:11:28 +0300

    Fix boolquery always returning results if at least one should clause matched

--- internal/index/query/bool.go
+++ internal/index/query/bool.go
@@ -87,28 +87,62 @@ func (q *boolQuery) exec(ctx context.Context) (*queryResult, error) {
 		return newEmptyResult(), nil
 	}
 
+	shouldResult, err := q.execType(ctx, q.should, func(src *queryResult, dst *queryResult) { src.Or(dst) })
+	if err != nil {
+		return newEmptyResult(), err
+	}
+
+	filterResult, err := q.execType(ctx, q.filter, func(src *queryResult, dst *queryResult) { src.And(dst) })
+	if err != nil {
+		return newEmptyResult(), err
+	}
+
+	mustResult, err := q.execType(ctx, q.must, func(src *queryResult, dst *queryResult) { src.And(dst) })
+	if err != nil {
+		return newEmptyResult(), err
+	}
+
 	var result *queryResult
-	for _, data := range []struct {
-		queries []internalQuery
-		apply   func(src *queryResult, dst *queryResult)
-	}{
-		{queries: q.filter, apply: func(src *queryResult, dst *queryResult) { src.And(dst) }},
-		{queries: q.must, apply: func(src *queryResult, dst *queryResult) { src.And(dst) }},
-		{queries: q.should, apply: func(src *queryResult, dst *queryResult) { src.Or(dst) }},
-	} {
-		for _, q := range data.queries {
-			r, err := q.exec(ctx)
-			if err != nil {
-				return newEmptyResult(), err
-			}
-
-			if result == nil {
-				result = r
-				continue
-			}
-
-			data.apply(result, r)
+	if shouldResult != nil {
+		result = shouldResult
+	}
+	if mustResult != nil {
+		if result == nil {
+			result = mustResult
+		} else {
+			result.And(mustResult)
 		}
+	}
+	if filterResult != nil {
+		if result == nil {
+			result = filterResult
+		} else {
+			result.And(filterResult)
+		}
+	}
+
+	return result, nil
+}
+
+func (q *boolQuery) execType(
+	ctx context.Context,
+	queries []internalQuery,
+	apply func(src *queryResult, dst *queryResult),
+) (*queryResult, error) {
+	var result *queryResult
+
+	for _, query := range queries {
+		r, err := query.exec(ctx)
+		if err != nil {
+			return newEmptyResult(), err
+		}
+
+		if result == nil {
+			result = r
+			continue
+		}
+
+		apply(result, r)
 	}
 
 	return result, nil
