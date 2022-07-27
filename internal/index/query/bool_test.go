83b8bfa6ee013e5effd4cf7ac0dc2dcf6410b95a
Dmitry Solovov <cyradin@protonmail.com>
2022-07-27 20:16:52 +0300

    Refactor query tests

--- internal/index/query/bool_test.go
+++ internal/index/query/bool_test.go
@@ -113,70 +113,69 @@ func bench_boolQuery(b *testing.B, counts []int, parallel bool) {
 	}
 }
 
-func Test_boolQuery(t *testing.T) {
-	t.Run("newBoolQuery", func(t *testing.T) {
-		t.Run("must not return error if request is an empty object", func(t *testing.T) {
-			query := "{}"
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+func Test_newBoolQuery(t *testing.T) {
+	t.Run("must not return error if request is an empty object", func(t *testing.T) {
+		query := "{}"
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newBoolQuery(context.Background(), req)
-			require.NoError(t, err)
-			require.NotNil(t, q)
-		})
-		t.Run("must return error if request contains extra keys", func(t *testing.T) {
-			query := `{
+		q, err := newBoolQuery(context.Background(), req)
+		require.NoError(t, err)
+		require.NotNil(t, q)
+	})
+	t.Run("must return error if request contains extra keys", func(t *testing.T) {
+		query := `{
 				"should": [],
 				"field": []
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newBoolQuery(context.Background(), req)
-			require.Error(t, err)
-			require.Nil(t, q)
-		})
-		t.Run("must return error if request field is not an array", func(t *testing.T) {
-			query := `{
+		q, err := newBoolQuery(context.Background(), req)
+		require.Error(t, err)
+		require.Nil(t, q)
+	})
+	t.Run("must return error if request field is not an array", func(t *testing.T) {
+		query := `{
 				"should": {
 					"query": "hello"
 				}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newBoolQuery(context.Background(), req)
-			require.Error(t, err)
-			require.Nil(t, q)
-		})
-		t.Run("must not return error if request field is an empty array", func(t *testing.T) {
-			query := `{
+		q, err := newBoolQuery(context.Background(), req)
+		require.Error(t, err)
+		require.Nil(t, q)
+	})
+	t.Run("must not return error if request field is an empty array", func(t *testing.T) {
+		query := `{
 				"should": []
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newBoolQuery(context.Background(), req)
-			require.NoError(t, err)
-			require.NotNil(t, q)
-		})
-		t.Run("must return error if request field contains invalid subquery", func(t *testing.T) {
-			query := `{
+		q, err := newBoolQuery(context.Background(), req)
+		require.NoError(t, err)
+		require.NotNil(t, q)
+	})
+	t.Run("must return error if request field contains invalid subquery", func(t *testing.T) {
+		query := `{
 				"should": [
 					{
 						"term": 1
 					}
 				]
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newBoolQuery(context.Background(), req)
-			require.Error(t, err)
-			require.Nil(t, q)
-		})
-		t.Run("must not return error if request is a valid query", func(t *testing.T) {
-			query := `{
+		q, err := newBoolQuery(context.Background(), req)
+		require.Error(t, err)
+		require.Nil(t, q)
+	})
+	t.Run("must not return error if request is a valid query", func(t *testing.T) {
+		query := `{
 				"should": [
 					{
 						"term": {
@@ -203,39 +202,39 @@ func Test_boolQuery(t *testing.T) {
 					}
 				]
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newBoolQuery(context.Background(), req)
-			require.Error(t, err)
-			require.Nil(t, q)
-		})
+		q, err := newBoolQuery(context.Background(), req)
+		require.Error(t, err)
+		require.Nil(t, q)
 	})
+}
 
-	t.Run("exec", func(t *testing.T) {
-		f1 := field.NewBool()
-		f1.Add(1, true)
-		f1.Add(2, false)
+func Test_boolQuery_exec(t *testing.T) {
+	f1 := field.NewBool()
+	f1.Add(1, true)
+	f1.Add(2, false)
 
-		f2 := field.NewAll()
-		f2.Add(1, true)
-		f2.Add(2, false)
-		f2.Add(3, true)
+	f2 := field.NewAll()
+	f2.Add(1, true)
+	f2.Add(2, false)
+	f2.Add(3, true)
 
-		ctx := withFields(context.Background(), map[string]field.Field{"field": f1, field.AllField: f2})
+	ctx := withFields(context.Background(), map[string]field.Field{"field": f1, field.AllField: f2})
 
-		t.Run("must return all documents for empty query", func(t *testing.T) {
-			query, err := decodeQuery(`{}`)
-			require.NoError(t, err)
-			bq, err := newBoolQuery(ctx, query)
-			require.NoError(t, err)
-			result, err := bq.exec(ctx)
-			require.NoError(t, err)
-			require.NotNil(t, result)
-			require.ElementsMatch(t, []uint32{1, 2, 3}, result.Docs().ToArray())
-		})
-		t.Run("must return documents for should query if documents match at least one query", func(t *testing.T) {
-			query, err := decodeQuery(`
+	t.Run("must return all documents for empty query", func(t *testing.T) {
+		query, err := decodeQuery(`{}`)
+		require.NoError(t, err)
+		bq, err := newBoolQuery(ctx, query)
+		require.NoError(t, err)
+		result, err := bq.exec(ctx)
+		require.NoError(t, err)
+		require.NotNil(t, result)
+		require.ElementsMatch(t, []uint32{1, 2, 3}, result.Docs().ToArray())
+	})
+	t.Run("must return documents for should query if documents match at least one query", func(t *testing.T) {
+		query, err := decodeQuery(`
 			{
 				"should": [
 					{
@@ -255,16 +254,16 @@ func Test_boolQuery(t *testing.T) {
 				]
 			}
 			`)
-			require.NoError(t, err)
-			bq, err := newBoolQuery(ctx, query)
-			require.NoError(t, err)
-			result, err := bq.exec(ctx)
-			require.NoError(t, err)
-			require.NotNil(t, result)
-			require.ElementsMatch(t, []uint32{1, 2}, result.Docs().ToArray())
-		})
-		t.Run("must return no documents for must query if no documents match all queries", func(t *testing.T) {
-			query, err := decodeQuery(`
+		require.NoError(t, err)
+		bq, err := newBoolQuery(ctx, query)
+		require.NoError(t, err)
+		result, err := bq.exec(ctx)
+		require.NoError(t, err)
+		require.NotNil(t, result)
+		require.ElementsMatch(t, []uint32{1, 2}, result.Docs().ToArray())
+	})
+	t.Run("must return no documents for must query if no documents match all queries", func(t *testing.T) {
+		query, err := decodeQuery(`
 			{
 				"must": [
 					{
@@ -284,16 +283,16 @@ func Test_boolQuery(t *testing.T) {
 				]
 			}
 			`)
-			require.NoError(t, err)
-			bq, err := newBoolQuery(ctx, query)
-			require.NoError(t, err)
-			result, err := bq.exec(ctx)
-			require.NoError(t, err)
-			require.NotNil(t, result)
-			require.ElementsMatch(t, []uint32{}, result.Docs().ToArray())
-		})
-		t.Run("must return documents for must query if some documents match all queries", func(t *testing.T) {
-			query, err := decodeQuery(`
+		require.NoError(t, err)
+		bq, err := newBoolQuery(ctx, query)
+		require.NoError(t, err)
+		result, err := bq.exec(ctx)
+		require.NoError(t, err)
+		require.NotNil(t, result)
+		require.ElementsMatch(t, []uint32{}, result.Docs().ToArray())
+	})
+	t.Run("must return documents for must query if some documents match all queries", func(t *testing.T) {
+		query, err := decodeQuery(`
 			{
 				"must": [
 					{
@@ -313,119 +312,16 @@ func Test_boolQuery(t *testing.T) {
 				]
 			}
 			`)
-			require.NoError(t, err)
-			bq, err := newBoolQuery(ctx, query)
-			require.NoError(t, err)
-			result, err := bq.exec(ctx)
-			require.NoError(t, err)
-			require.NotNil(t, result)
-			require.ElementsMatch(t, []uint32{1}, result.Docs().ToArray())
-		})
-		t.Run("must return no documents for must+should query if no documents match all queries", func(t *testing.T) {
-			query, err := decodeQuery(`
-			{
-				"should": [
-					{
-						"term": {
-							"field": {
-								"query": true
-							}
-						}
-					},
-					{
-						"term": {
-							"field": {
-								"query": false
-							}
-						}
-					}
-				],
-				"must": [
-					{
-						"term": {
-							"field": {
-								"query": true
-							}
-						}
-					},
-					{
-						"term": {
-							"field": {
-								"query": false
-							}
-						}
-					}
-				]
-			}
-			`)
-			require.NoError(t, err)
-			bq, err := newBoolQuery(ctx, query)
-			require.NoError(t, err)
-			result, err := bq.exec(ctx)
-			require.NoError(t, err)
-			require.NotNil(t, result)
-			require.ElementsMatch(t, []uint32{}, result.Docs().ToArray())
-		})
-		t.Run("must return no documents for filter query if no documents match all queries", func(t *testing.T) {
-			query, err := decodeQuery(`
-			{
-				"filter": [
-					{
-						"term": {
-							"field": {
-								"query": true
-							}
-						}
-					},
-					{
-						"term": {
-							"field": {
-								"query": false
-							}
-						}
-					}
-				]
-			}
-			`)
-			require.NoError(t, err)
-			bq, err := newBoolQuery(ctx, query)
-			require.NoError(t, err)
-			result, err := bq.exec(ctx)
-			require.NoError(t, err)
-			require.NotNil(t, result)
-			require.ElementsMatch(t, []uint32{}, result.Docs().ToArray())
-		})
-		t.Run("must return documents for filter query if some documents match all queries", func(t *testing.T) {
-			query, err := decodeQuery(`
-			{
-				"filter": [
-					{
-						"term": {
-							"field": {
-								"query": true
-							}
-						}
-					},
-					{
-						"term": {
-							"field": {
-								"query": true
-							}
-						}
-					}
-				]
-			}
-			`)
-			require.NoError(t, err)
-			bq, err := newBoolQuery(ctx, query)
-			require.NoError(t, err)
-			result, err := bq.exec(ctx)
-			require.NoError(t, err)
-			require.NotNil(t, result)
-			require.ElementsMatch(t, []uint32{1}, result.Docs().ToArray())
-		})
-		t.Run("must return no documents for filter+should query if no documents match all queries", func(t *testing.T) {
-			query, err := decodeQuery(`
+		require.NoError(t, err)
+		bq, err := newBoolQuery(ctx, query)
+		require.NoError(t, err)
+		result, err := bq.exec(ctx)
+		require.NoError(t, err)
+		require.NotNil(t, result)
+		require.ElementsMatch(t, []uint32{1}, result.Docs().ToArray())
+	})
+	t.Run("must return no documents for must+should query if no documents match all queries", func(t *testing.T) {
+		query, err := decodeQuery(`
 			{
 				"should": [
 					{
@@ -461,13 +357,115 @@ func Test_boolQuery(t *testing.T) {
 				]
 			}
 			`)
-			require.NoError(t, err)
-			bq, err := newBoolQuery(ctx, query)
-			require.NoError(t, err)
-			result, err := bq.exec(ctx)
-			require.NoError(t, err)
-			require.NotNil(t, result)
-			require.ElementsMatch(t, []uint32{}, result.Docs().ToArray())
-		})
+		require.NoError(t, err)
+		bq, err := newBoolQuery(ctx, query)
+		require.NoError(t, err)
+		result, err := bq.exec(ctx)
+		require.NoError(t, err)
+		require.NotNil(t, result)
+		require.ElementsMatch(t, []uint32{}, result.Docs().ToArray())
+	})
+	t.Run("must return no documents for filter query if no documents match all queries", func(t *testing.T) {
+		query, err := decodeQuery(`
+			{
+				"filter": [
+					{
+						"term": {
+							"field": {
+								"query": true
+							}
+						}
+					},
+					{
+						"term": {
+							"field": {
+								"query": false
+							}
+						}
+					}
+				]
+			}
+			`)
+		require.NoError(t, err)
+		bq, err := newBoolQuery(ctx, query)
+		require.NoError(t, err)
+		result, err := bq.exec(ctx)
+		require.NoError(t, err)
+		require.NotNil(t, result)
+		require.ElementsMatch(t, []uint32{}, result.Docs().ToArray())
+	})
+	t.Run("must return documents for filter query if some documents match all queries", func(t *testing.T) {
+		query, err := decodeQuery(`
+			{
+				"filter": [
+					{
+						"term": {
+							"field": {
+								"query": true
+							}
+						}
+					},
+					{
+						"term": {
+							"field": {
+								"query": true
+							}
+						}
+					}
+				]
+			}
+			`)
+		require.NoError(t, err)
+		bq, err := newBoolQuery(ctx, query)
+		require.NoError(t, err)
+		result, err := bq.exec(ctx)
+		require.NoError(t, err)
+		require.NotNil(t, result)
+		require.ElementsMatch(t, []uint32{1}, result.Docs().ToArray())
+	})
+	t.Run("must return no documents for filter+should query if no documents match all queries", func(t *testing.T) {
+		query, err := decodeQuery(`
+			{
+				"should": [
+					{
+						"term": {
+							"field": {
+								"query": true
+							}
+						}
+					},
+					{
+						"term": {
+							"field": {
+								"query": false
+							}
+						}
+					}
+				],
+				"must": [
+					{
+						"term": {
+							"field": {
+								"query": true
+							}
+						}
+					},
+					{
+						"term": {
+							"field": {
+								"query": false
+							}
+						}
+					}
+				]
+			}
+			`)
+		require.NoError(t, err)
+		bq, err := newBoolQuery(ctx, query)
+		require.NoError(t, err)
+		result, err := bq.exec(ctx)
+		require.NoError(t, err)
+		require.NotNil(t, result)
+		require.ElementsMatch(t, []uint32{}, result.Docs().ToArray())
 	})
 }
--- internal/index/query/term_test.go
+++ internal/index/query/term_test.go
@@ -8,268 +8,264 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_termQuery(t *testing.T) {
-	t.Run("newTermQuery", func(t *testing.T) {
-		t.Run("must return error if request is an empty object", func(t *testing.T) {
-			query := "{}"
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+func Test_newTermQuery(t *testing.T) {
+	t.Run("must return error if request is an empty object", func(t *testing.T) {
+		query := "{}"
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newTermQuery(context.Background(), req)
-			require.Error(t, err)
-			require.Nil(t, q)
-		})
-		t.Run("must return error if request contains multiple keys", func(t *testing.T) {
-			query := `{
+		q, err := newTermQuery(context.Background(), req)
+		require.Error(t, err)
+		require.Nil(t, q)
+	})
+	t.Run("must return error if request contains multiple keys", func(t *testing.T) {
+		query := `{
 				"field1": {},
 				"field2": {}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newTermQuery(context.Background(), req)
-			require.Error(t, err)
-			require.Nil(t, q)
-		})
-		t.Run("must return error if request field is empty", func(t *testing.T) {
-			query := `{
+		q, err := newTermQuery(context.Background(), req)
+		require.Error(t, err)
+		require.Nil(t, q)
+	})
+	t.Run("must return error if request field is empty", func(t *testing.T) {
+		query := `{
 				"field1": {}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newTermQuery(context.Background(), req)
-			require.Error(t, err)
-			require.Nil(t, q)
-		})
-		t.Run("must return error if request field contains extra keys", func(t *testing.T) {
-			query := `{
+		q, err := newTermQuery(context.Background(), req)
+		require.Error(t, err)
+		require.Nil(t, q)
+	})
+	t.Run("must return error if request field contains extra keys", func(t *testing.T) {
+		query := `{
 				"field1": {
 					"query": "hello",
 					"qwerty": "hello"
 				}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newTermQuery(context.Background(), req)
-			require.Error(t, err)
-			require.Nil(t, q)
-		})
-		t.Run("must not return error if request is a valid query", func(t *testing.T) {
-			query := `{
+		q, err := newTermQuery(context.Background(), req)
+		require.Error(t, err)
+		require.Nil(t, q)
+	})
+	t.Run("must not return error if request is a valid query", func(t *testing.T) {
+		query := `{
 				"field1": {
 					"query": "hello"
 				}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newTermQuery(context.Background(), req)
-			require.NoError(t, err)
-			require.NotNil(t, q)
-		})
+		q, err := newTermQuery(context.Background(), req)
+		require.NoError(t, err)
+		require.NotNil(t, q)
 	})
+}
 
-	t.Run("exec", func(t *testing.T) {
-		f := field.NewKeyword()
-		f.Add(1, "value")
-		ctx := withFields(context.Background(),
-			map[string]field.Field{
-				"field": f,
-			},
-		)
+func Test_termQuery_exec(t *testing.T) {
+	f := field.NewKeyword()
+	f.Add(1, "value")
+	ctx := withFields(context.Background(),
+		map[string]field.Field{
+			"field": f,
+		},
+	)
 
-		t.Run("must return empty result if field not found", func(t *testing.T) {
-			query := `{
+	t.Run("must return empty result if field not found", func(t *testing.T) {
+		query := `{
 				"field1": {
 					"query": "value"
 				}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			tq, err := newTermQuery(ctx, req)
-			require.NoError(t, err)
+		tq, err := newTermQuery(ctx, req)
+		require.NoError(t, err)
 
-			result, err := tq.exec(ctx)
-			require.NoError(t, err)
-			require.True(t, result.Docs().IsEmpty())
-		})
+		result, err := tq.exec(ctx)
+		require.NoError(t, err)
+		require.True(t, result.Docs().IsEmpty())
+	})
 
-		t.Run("must return empty result if value not found", func(t *testing.T) {
-			query := `{
+	t.Run("must return empty result if value not found", func(t *testing.T) {
+		query := `{
 				"field": {
 					"query": "value1"
 				}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			tq, err := newTermQuery(ctx, req)
-			require.NoError(t, err)
+		tq, err := newTermQuery(ctx, req)
+		require.NoError(t, err)
 
-			result, err := tq.exec(ctx)
-			require.NoError(t, err)
-			require.True(t, result.Docs().IsEmpty())
-		})
+		result, err := tq.exec(ctx)
+		require.NoError(t, err)
+		require.True(t, result.Docs().IsEmpty())
+	})
 
-		t.Run("must return non-empty result if value is found", func(t *testing.T) {
-			query := `{
+	t.Run("must return non-empty result if value is found", func(t *testing.T) {
+		query := `{
 				"field": {
 					"query": "value"
 				}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			tq, err := newTermQuery(ctx, req)
-			require.NoError(t, err)
+		tq, err := newTermQuery(ctx, req)
+		require.NoError(t, err)
 
-			result, err := tq.exec(ctx)
-			require.NoError(t, err)
+		result, err := tq.exec(ctx)
+		require.NoError(t, err)
 
-			require.False(t, result.Docs().IsEmpty())
-			require.ElementsMatch(t, []uint32{1}, result.Docs().ToArray())
-		})
+		require.False(t, result.Docs().IsEmpty())
+		require.ElementsMatch(t, []uint32{1}, result.Docs().ToArray())
 	})
 }
 
-func Test_termsQuery(t *testing.T) {
-	t.Run("newTermsQuery", func(t *testing.T) {
-		t.Run("must return error if request is an empty object", func(t *testing.T) {
-			query := "{}"
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+func Test_newTermsQuery(t *testing.T) {
+	t.Run("must return error if request is an empty object", func(t *testing.T) {
+		query := "{}"
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newTermsQuery(context.Background(), req)
-			require.Error(t, err)
-			require.Nil(t, q)
-		})
-		t.Run("must return error if request contains multiple keys", func(t *testing.T) {
-			query := `{
+		q, err := newTermsQuery(context.Background(), req)
+		require.Error(t, err)
+		require.Nil(t, q)
+	})
+	t.Run("must return error if request contains multiple keys", func(t *testing.T) {
+		query := `{
 				"field1": {},
 				"field2": {}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newTermsQuery(context.Background(), req)
-			require.Error(t, err)
-			require.Nil(t, q)
-		})
-		t.Run("must return error if request field is empty", func(t *testing.T) {
-			query := `{
+		q, err := newTermsQuery(context.Background(), req)
+		require.Error(t, err)
+		require.Nil(t, q)
+	})
+	t.Run("must return error if request field is empty", func(t *testing.T) {
+		query := `{
 				"field1": {}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newTermsQuery(context.Background(), req)
-			require.Error(t, err)
-			require.Nil(t, q)
-		})
-		t.Run("must return error if request field contains extra keys", func(t *testing.T) {
-			query := `{
+		q, err := newTermsQuery(context.Background(), req)
+		require.Error(t, err)
+		require.Nil(t, q)
+	})
+	t.Run("must return error if request field contains extra keys", func(t *testing.T) {
+		query := `{
 				"field1": {
 					"query": "hello",
 					"qwerty": "hello"
 				}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newTermsQuery(context.Background(), req)
-			require.Error(t, err)
-			require.Nil(t, q)
-		})
-		t.Run("must return error if request query is not an array", func(t *testing.T) {
-			query := `{
+		q, err := newTermsQuery(context.Background(), req)
+		require.Error(t, err)
+		require.Nil(t, q)
+	})
+	t.Run("must return error if request query is not an array", func(t *testing.T) {
+		query := `{
 				"field1": {
 					"query": "hello"
 				}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newTermsQuery(context.Background(), req)
-			require.Error(t, err)
-			require.Nil(t, q)
-		})
-		t.Run("must not return error if request query is a valid query", func(t *testing.T) {
-			query := `{
+		q, err := newTermsQuery(context.Background(), req)
+		require.Error(t, err)
+		require.Nil(t, q)
+	})
+	t.Run("must not return error if request query is a valid query", func(t *testing.T) {
+		query := `{
 				"field1": {
 					"query": ["hello"]
 				}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			q, err := newTermsQuery(context.Background(), req)
-			require.NoError(t, err)
-			require.NotNil(t, q)
-		})
+		q, err := newTermsQuery(context.Background(), req)
+		require.NoError(t, err)
+		require.NotNil(t, q)
 	})
+}
 
-	t.Run("exec", func(t *testing.T) {
-		f := field.NewKeyword()
-		f.Add(1, "value")
-		ctx := withFields(context.Background(),
-			map[string]field.Field{
-				"field": f,
-			},
-		)
+func Test_termsQuery_exec(t *testing.T) {
+	f := field.NewKeyword()
+	f.Add(1, "value")
+	ctx := withFields(context.Background(),
+		map[string]field.Field{
+			"field": f,
+		},
+	)
 
-		t.Run("must return empty result if field not found", func(t *testing.T) {
-			query := `{
+	t.Run("must return empty result if field not found", func(t *testing.T) {
+		query := `{
 				"field1": {
 					"query": ["value1"]
 				}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			tq, err := newTermsQuery(ctx, req)
-			require.NoError(t, err)
+		tq, err := newTermsQuery(ctx, req)
+		require.NoError(t, err)
 
-			result, err := tq.exec(ctx)
-			require.NoError(t, err)
-			require.True(t, result.Docs().IsEmpty())
-		})
+		result, err := tq.exec(ctx)
+		require.NoError(t, err)
+		require.True(t, result.Docs().IsEmpty())
+	})
 
-		t.Run("must return empty result if value not found", func(t *testing.T) {
-			query := `{
+	t.Run("must return empty result if value not found", func(t *testing.T) {
+		query := `{
 				"field": {
 					"query": ["value1"]
 				}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			tq, err := newTermsQuery(ctx, req)
-			require.NoError(t, err)
+		tq, err := newTermsQuery(ctx, req)
+		require.NoError(t, err)
 
-			result, err := tq.exec(ctx)
-			require.NoError(t, err)
-			require.True(t, result.Docs().IsEmpty())
-		})
+		result, err := tq.exec(ctx)
+		require.NoError(t, err)
+		require.True(t, result.Docs().IsEmpty())
+	})
 
-		t.Run("must return non-empty result if value is found", func(t *testing.T) {
-			query := `{
+	t.Run("must return non-empty result if value is found", func(t *testing.T) {
+		query := `{
 				"field": {
 					"query": ["value"]
 				}
 			}`
-			req, err := decodeQuery(query)
-			require.NoError(t, err)
+		req, err := decodeQuery(query)
+		require.NoError(t, err)
 
-			tq, err := newTermsQuery(ctx, req)
-			require.NoError(t, err)
+		tq, err := newTermsQuery(ctx, req)
+		require.NoError(t, err)
 
-			result, err := tq.exec(ctx)
-			require.NoError(t, err)
-			require.False(t, result.Docs().IsEmpty())
-			require.ElementsMatch(t, []uint32{1}, result.Docs().ToArray())
-		})
+		result, err := tq.exec(ctx)
+		require.NoError(t, err)
+		require.False(t, result.Docs().IsEmpty())
+		require.ElementsMatch(t, []uint32{1}, result.Docs().ToArray())
 	})
 }
