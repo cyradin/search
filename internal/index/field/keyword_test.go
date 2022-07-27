b47a1252b22d4244c3eb5c7cb3e1739c88c0fa7a
Dmitry Solovov <cyradin@protonmail.com>
2022-07-27 19:42:48 +0300

    Refactor field tests

--- internal/index/field/keyword_test.go
+++ internal/index/field/keyword_test.go
@@ -7,90 +7,88 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func Test_Keyword(t *testing.T) {
-	t.Run("Add", func(t *testing.T) {
-		t.Run("bool", func(t *testing.T) {
-			field := NewKeyword()
-			field.Add(1, true)
-
-			require.EqualValues(t, 1, field.data["true"].GetCardinality())
-			require.True(t, field.data["true"].Contains(1))
-		})
-		t.Run("string", func(t *testing.T) {
-			field := NewKeyword()
-			field.Add(1, "foo")
-			field.Add(1, "bar")
-			field.Add(2, "foo")
-
-			require.EqualValues(t, 2, field.data["foo"].GetCardinality())
-			require.EqualValues(t, 1, field.data["bar"].GetCardinality())
-			require.True(t, field.data["foo"].Contains(1))
-			require.True(t, field.data["foo"].Contains(2))
-			require.True(t, field.data["bar"].Contains(1))
-			require.False(t, field.data["bar"].Contains(2))
-		})
-	})
-
-	t.Run("Term", func(t *testing.T) {
+func Test_Keyword_Add(t *testing.T) {
+	t.Run("bool", func(t *testing.T) {
 		field := NewKeyword()
-		field.Add(1, "foo")
+		field.Add(1, true)
 
-		result := field.Term(context.Background(), "foo")
-		require.True(t, result.Docs().Contains(1))
-		require.EqualValues(t, 1, result.Docs().GetCardinality())
-
-		result = field.Term(context.Background(), "bar")
-		require.False(t, result.Docs().Contains(1))
-		require.EqualValues(t, 0, result.Docs().GetCardinality())
+		require.EqualValues(t, 1, field.data["true"].GetCardinality())
+		require.True(t, field.data["true"].Contains(1))
 	})
-
-	t.Run("Delete", func(t *testing.T) {
+	t.Run("string", func(t *testing.T) {
 		field := NewKeyword()
 		field.Add(1, "foo")
 		field.Add(1, "bar")
 		field.Add(2, "foo")
 
-		field.Delete(2)
-		require.EqualValues(t, 1, field.data["foo"].GetCardinality())
+		require.EqualValues(t, 2, field.data["foo"].GetCardinality())
 		require.EqualValues(t, 1, field.data["bar"].GetCardinality())
-		require.EqualValues(t, map[string]struct{}{"foo": {}, "bar": {}}, field.values[1])
-		require.Nil(t, field.values[2])
-
-		field.Delete(1)
-		require.Nil(t, field.data["foo"])
-		require.Nil(t, field.data["bar"])
-		require.Nil(t, field.values[1])
+		require.True(t, field.data["foo"].Contains(1))
+		require.True(t, field.data["foo"].Contains(2))
+		require.True(t, field.data["bar"].Contains(1))
+		require.False(t, field.data["bar"].Contains(2))
 	})
+}
 
-	t.Run("Data", func(t *testing.T) {
-		field := NewKeyword()
-		field.Add(1, "foo")
-		field.Add(1, "bar")
-		field.Add(2, "foo")
+func Test_Keyword_Term(t *testing.T) {
+	field := NewKeyword()
+	field.Add(1, "foo")
 
-		result := field.Data(1)
-		require.ElementsMatch(t, []string{"foo", "bar"}, result)
+	result := field.Term(context.Background(), "foo")
+	require.True(t, result.Docs().Contains(1))
+	require.EqualValues(t, 1, result.Docs().GetCardinality())
 
-		result = field.Data(2)
-		require.ElementsMatch(t, []string{"foo"}, result)
-	})
+	result = field.Term(context.Background(), "bar")
+	require.False(t, result.Docs().Contains(1))
+	require.EqualValues(t, 0, result.Docs().GetCardinality())
+}
 
-	t.Run("MarshalBinary-UnmarshalBinary", func(t *testing.T) {
-		field := NewKeyword()
-		field.Add(1, "foo")
-		field.Add(1, "bar")
-		field.Add(2, "foo")
+func Test_Keyword_Delete(t *testing.T) {
+	field := NewKeyword()
+	field.Add(1, "foo")
+	field.Add(1, "bar")
+	field.Add(2, "foo")
 
-		data, err := field.MarshalBinary()
-		require.NoError(t, err)
+	field.Delete(2)
+	require.EqualValues(t, 1, field.data["foo"].GetCardinality())
+	require.EqualValues(t, 1, field.data["bar"].GetCardinality())
+	require.EqualValues(t, map[string]struct{}{"foo": {}, "bar": {}}, field.values[1])
+	require.Nil(t, field.values[2])
 
-		field2 := NewKeyword()
-		err = field2.UnmarshalBinary(data)
-		require.NoError(t, err)
-		require.True(t, field2.data["foo"].Contains(1))
-		require.True(t, field2.data["bar"].Contains(1))
-		require.EqualValues(t, map[string]struct{}{"foo": {}, "bar": {}}, field.values[1])
-		require.True(t, field2.data["foo"].Contains(2))
-		require.EqualValues(t, map[string]struct{}{"foo": {}}, field.values[2])
-	})
+	field.Delete(1)
+	require.Nil(t, field.data["foo"])
+	require.Nil(t, field.data["bar"])
+	require.Nil(t, field.values[1])
+}
+
+func Test_Keyword_Data(t *testing.T) {
+	field := NewKeyword()
+	field.Add(1, "foo")
+	field.Add(1, "bar")
+	field.Add(2, "foo")
+
+	result := field.Data(1)
+	require.ElementsMatch(t, []string{"foo", "bar"}, result)
+
+	result = field.Data(2)
+	require.ElementsMatch(t, []string{"foo"}, result)
+}
+
+func Test_Keyword_Marshal(t *testing.T) {
+	field := NewKeyword()
+	field.Add(1, "foo")
+	field.Add(1, "bar")
+	field.Add(2, "foo")
+
+	data, err := field.MarshalBinary()
+	require.NoError(t, err)
+
+	field2 := NewKeyword()
+	err = field2.UnmarshalBinary(data)
+	require.NoError(t, err)
+	require.True(t, field2.data["foo"].Contains(1))
+	require.True(t, field2.data["bar"].Contains(1))
+	require.EqualValues(t, map[string]struct{}{"foo": {}, "bar": {}}, field.values[1])
+	require.True(t, field2.data["foo"].Contains(2))
+	require.EqualValues(t, map[string]struct{}{"foo": {}}, field.values[2])
 }
