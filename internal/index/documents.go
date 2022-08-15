86c39f0c215e8d3c4fcd96defabf2aa2ae7f58cc
Dmitry Solovov <cyradin@protonmail.com>
2022-08-16 02:17:57 +0300

    Add request agg fields. Execute request aggs

--- internal/index/documents.go
+++ internal/index/documents.go
@@ -1,44 +1,12 @@
 package index
 
 import (
-	"context"
-	"time"
-
 	"github.com/cyradin/search/internal/errs"
 	"github.com/cyradin/search/internal/index/field"
-	"github.com/cyradin/search/internal/index/query"
 	"github.com/cyradin/search/internal/index/schema"
-	jsoniter "github.com/json-iterator/go"
 )
 
 type DocSource map[string]interface{}
-
-type Search struct {
-	Query  jsoniter.RawMessage `json:"query"`
-	Limit  int                 `json:"limit"`
-	Offset int                 `json:"offset"`
-}
-
-type SearchResult struct {
-	Took int        `json:"took"`
-	Hits SearchHits `json:"hits"`
-}
-
-type SearchHits struct {
-	Total    SearchTotal `json:"total"`
-	Hits     []SearchHit `json:"hits"`
-	MaxScore float64     `json:"maxScore"`
-}
-
-type SearchTotal struct {
-	Value    int    `json:"value"`
-	Relation string `json:"relation"`
-}
-
-type SearchHit struct {
-	ID    uint32  `json:"id"`
-	Score float64 `json:"score"`
-}
 
 type Documents struct {
 	fields *field.Storage
@@ -116,53 +84,4 @@ func (d *Documents) Delete(index Index, id uint32) error {
 	fieldIndex.Delete(id)
 
 	return nil
-}
-
-func (d *Documents) Search(ctx context.Context, index Index, q Search) (SearchResult, error) {
-	fieldIndex, err := d.fields.GetIndex(index.Name)
-	if err != nil {
-		return SearchResult{}, err
-	}
-
-	qb, err := query.Build(query.QueryRequest(q.Query))
-	if err != nil {
-		return SearchResult{}, err
-	}
-
-	t := time.Now()
-	qr, err := qb.Exec(ctx, fieldIndex.Fields())
-	if err != nil {
-		return SearchResult{}, err
-	}
-	took := time.Since(t).Microseconds()
-
-	if err != nil {
-		return SearchResult{}, err
-	}
-
-	total := qr.Docs().GetCardinality()
-	hits := make([]SearchHit, total)
-	maxScore := 0.0
-	for i, id := range qr.Docs().ToArray() {
-		score := qr.Score(id)
-		if maxScore < score {
-			maxScore = score
-		}
-		hits[i] = SearchHit{
-			ID:    id,
-			Score: score,
-		}
-	}
-
-	return SearchResult{
-		Hits: SearchHits{
-			Total: SearchTotal{
-				Value:    int(total),
-				Relation: "eq",
-			},
-			Hits:     hits,
-			MaxScore: maxScore,
-		},
-		Took: int(took),
-	}, nil
 }
